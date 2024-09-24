8da5333f4924706103bb7ca2478b4c8956943449
Victor <viictorvaalah@gmail.com>
2024-09-24 19:54:19 +0100

    Added new features

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,18 @@
-ECHO is on.
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://qwertyuiop1234567890:qwertyuiop1234567890@cluster0.8hza9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+
